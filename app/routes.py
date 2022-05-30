f79056f599ac8fd19119bb268314ba33705b9b29
Alen-Berg <alexei.hlatsko@gmail.com>
2022-05-30 15:14:29 +0200

    homework 27-05-2022 AH

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,21 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-@app.route('/index/<name>')
-def index(name="Hello World"):
-    return render_template("index.html.jinja", text=name)
+def index():
+    return render_template("index.html")
+
+@app.route('/extraction')
+def extraction():
+    return render_template("extraction.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('not_found.html')
